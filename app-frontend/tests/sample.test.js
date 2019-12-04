2f6495e3577b4f4dd83d47f1b70a82f8aab2081b
omrilevi1994 <omri.levi@logz.io>
2019-12-04 16:05:37 +0200

    da

--- app-frontend/tests/sample.test.js
+++ app-frontend/tests/sample.test.js
@@ -3,3 +3,19 @@ describe('app-frontend', () => {
     expect(true).toBe(true)
   })
 })
+
+describe('app-frontend', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+describe('app-frontend', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+describe('app-frontend', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
